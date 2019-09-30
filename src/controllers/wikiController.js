827b0e05fa189f203bca445d93cdea9931652a85
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2019-09-29 23:59:55 -0500

    Markdown complete

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,5 +1,6 @@
 const wikiQueries = require("../db/queries.wikis.js");
 const Authorizer = require("../policies/wiki");
+const markdown = require( "markdown" ).markdown;
 
 module.exports = {
 	index(req, res, next){
@@ -44,6 +45,7 @@ module.exports = {
             if(err || wiki == null){
                 res.redirect(404, '/');
             } else {
+                wiki.body = markdown.toHTML(wiki.body);
                 res.render('wikis/show', {wiki});
             }
         });
